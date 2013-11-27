21e0c9934f03024a571ee273953797aa818690dc
nipil <nipil@users.noreply.github.com>
2013-11-27 19:00:19 +0100

    http-rpc working with auth and token

--- src/rpc.cpp
+++ src/rpc.cpp
@@ -4,6 +4,7 @@
 
 Rpc::Rpc(QObject * p, QSettings * s) :
     QObject(p),
+    requires_auth(false),
     settings(s)
 {
     nam = new QNetworkAccessManager(this);
@@ -27,19 +28,28 @@ void Rpc::request(QString request)
         url.setScheme("https");
     else
         url.setScheme("http");
-    url.setPath("/transmission/rpc");
+    url.setPath("/transmission/rpc/");
     qDebug() << "url" << url;
 
     QNetworkRequest * req = new QNetworkRequest(url);
 
-    QString username = settings->value(TTS_SETTINGS_RPC_USER).toString();
-    QString password = settings->value(TTS_SETTINGS_RPC_PASSWORD).toString();
-    if (!username.isEmpty() && !password.isEmpty())
+    req->setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    QString ua = TTS_APP_NAME + QString("/") + TTS_APP_VERSION;
+    req->setRawHeader(QByteArray("User-Agent"), QByteArray(ua.toUtf8()));
+
+    if (requires_auth)
     {
-        QByteArray basic_auth = QByteArray(QString(username + ":" + password).toUtf8()).toBase64();
-        qDebug() << "username" << username << "password" << password << "basic_auth" << basic_auth;
-        req->setRawHeader(QByteArray("Authorization"),basic_auth);
+        QString username = settings->value(TTS_SETTINGS_RPC_USER).toString();
+        QString password = settings->value(TTS_SETTINGS_RPC_PASSWORD).toString();
+        if (!username.isEmpty() && !password.isEmpty())
+        {
+            QByteArray basic_auth = "Basic " + QByteArray(QString(username + ":" + password).toUtf8()).toBase64();
+            qDebug() << "With authentication: username" << username << "password" << password << "basic_auth" << basic_auth;
+            req->setRawHeader(QByteArray("Authorization"),basic_auth);
+        }
     }
+    else
+        qDebug() << "Without authentication";
 
     if (!auth_token.isEmpty())
     {
@@ -48,6 +58,7 @@ void Rpc::request(QString request)
     }
 
     QNetworkReply* reply = nam->post(*req,request.toUtf8());
+    requests.insert(reply,request);
 
     if (req) delete req;
 }
@@ -56,8 +67,48 @@ void Rpc::finished( QNetworkReply * reply )
 {
     qDebug() << "RPC finished";
 
-    if (reply->error() != QNetworkReply::NoError)
+    QString cur_request = requests.take(reply);
+    if (cur_request.isEmpty())
+        qDebug() << "No request found for hash" << reply;
+
+    switch (reply->error())
+    {
+    case QNetworkReply::NoError:
+        // 0 = no error condition.
+        // Note: When the HTTP protocol returns a redirect no error will be reported. You can check if there is a redirect with the QNetworkRequest::RedirectionTargetAttribute attribute.
+        break;
+    case QNetworkReply::AuthenticationRequiredError:
+        // 204 = the remote server requires authentication to serve the content
+        // but the credentials provided were not accepted (if any)
+        // corresponds to the HTTP 401 error
+        if (!requires_auth)
+        {
+            qDebug() << "Authentication required, retrying with credentials";
+            requires_auth = true;
+            request(cur_request);
+        }
+        else
+        {
+            qCritical() << "Authentication failed" << reply;
+        }
+        break;
+    case QNetworkReply::UnknownContentError:
+        // 299 = an unknown error related to the remote content was detected
+        // corresponds to the HTTP 409 Conflict
+        // reply provides the X-Transmission-Session-Id header
+        auth_token = reply->rawHeader(QByteArray("X-Transmission-Session-Id"));
+        if (auth_token.isEmpty())
+        {
+            qDebug() << "Didn't get an X-Transmission-Session-Id token." << reply;
+            break;
+        }
+        qDebug() << "X-Transmission-Session-Id" << auth_token << "now redo request";
+        request(cur_request);
+        break;
+    default:
         qDebug() << "Error" << reply->error();
+        break;
+    }
 
     if (reply) reply->deleteLater();
 }
