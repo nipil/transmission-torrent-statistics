260f7b61efe3e0ad604a041c0b87df8fe7cb95f7
nipil <nipil@users.noreply.github.com>
2013-12-17 17:27:56 +0100

    missing declaration in previous commit
    see commit 30faedae5f4d38bf7c25b4ee9ad6a5bf5ebb248d

--- src/options.h
+++ src/options.h
@@ -10,6 +10,7 @@ class Options
 
     void errorUnknown(QString arg);
     void errorMissing(QString arg);
+    void errorInvalid(QString arg);
     void errorConvert(QString arg, QString type);
     void errorRange(QString arg, QString v_min, QString v_max);
 
